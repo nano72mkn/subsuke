36c3141c74ca99053f7b9954b857d4322f08343b
nano72mkn <shota+github@code-lab.xyz>
2024-12-27 17:53:29 +0900

    pwaをインストール

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { remixPWA } from '@remix-pwa/dev';
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -20,5 +21,6 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    remixPWA(),
   ],
 });
