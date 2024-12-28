8e491ce484ceb40f3f5cf3950fd36dc2df1c2e23
nano72mkn <shota+github@code-lab.xyz>
2024-12-28 20:28:03 +0900

    headerのデザイン調整

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,9 +6,9 @@ import { Button } from "./ui/button";
 export const Header = () => {
   return <div className="flex justify-between items-center mb-6">
     <h1 className="text-3xl font-bold">{siteConfig.title}</h1>
-    <div className="flex gap-4 items-center">
-      <Link to="/">サブスクリスト</Link>
-      <Link to="/dashboard">ダッシュボード</Link>
+    <div className="flex gap-6 items-center">
+      <Link to="/" className="font-bold text-sm">サブスクリスト</Link>
+      <Link to="/dashboard" className="font-bold text-sm">ダッシュボード</Link>
       <Link to="/new">
         <Button>
           <PlusCircle className="mr-2 h-4 w-4" />
