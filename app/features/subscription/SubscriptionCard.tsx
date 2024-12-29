e9f72b5897c25740a520be5766689f7c1f6fb281
nano72mkn <shota+github@code-lab.xyz>
2024-12-29 14:51:34 +0900

    削除の前にアラート

--- app/features/subscription/SubscriptionCard.tsx
+++ app/features/subscription/SubscriptionCard.tsx
@@ -1,4 +1,5 @@
 import { EllipsisVertical, Trash2 } from "lucide-react";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "~/components/ui/alert-dialog";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent } from "~/components/ui/card";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "~/components/ui/dropdown-menu";
@@ -31,22 +32,38 @@ export function SubscriptionCard({ sub, deleteSubscription }: SubscriptionCardPr
               {categoryOptions.find((category) => category.id === sub.category)?.label}
             </p>
           </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Button
-                variant="ghost"
-                size="icon"
-              >
-                <EllipsisVertical size={16} />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem onClick={() => deleteSubscription(sub.id)}>
-                <Trash2 size={16} />
-                <span>削除</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <AlertDialog>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                >
+                  <EllipsisVertical size={16} />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <AlertDialogTrigger asChild>
+                  <DropdownMenuItem>
+                    <Trash2 size={16} />
+                    <span>削除</span>
+                  </DropdownMenuItem>
+                </AlertDialogTrigger>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>{ sub.name }を削除しますか？</AlertDialogTitle>
+                <AlertDialogDescription>
+                  削除すると復元できませんがよろしいですか？
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>キャンセル</AlertDialogCancel>
+                <AlertDialogAction onClick={() => deleteSubscription(sub.id)}>削除する</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </div>
       </CardContent>
     </Card>
