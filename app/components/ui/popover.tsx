63e2dc4e67b40b176da756da01ec1c2a44a14f39
nano72mkn <shota+github@code-lab.xyz>
2025-01-04 02:09:47 +0900

    ポータルを消してみた

--- app/components/ui/popover.tsx
+++ app/components/ui/popover.tsx
@@ -1,5 +1,5 @@
-import * as React from "react"
 import * as PopoverPrimitive from "@radix-ui/react-popover"
+import * as React from "react"
 
 import { cn } from "~/lib/utils"
 
@@ -11,7 +11,6 @@ const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
 >(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
-  <PopoverPrimitive.Portal>
     <PopoverPrimitive.Content
       ref={ref}
       align={align}
@@ -22,8 +21,8 @@ const PopoverContent = React.forwardRef<
       )}
       {...props}
     />
-  </PopoverPrimitive.Portal>
 ))
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
 
-export { Popover, PopoverTrigger, PopoverContent }
+export { Popover, PopoverContent, PopoverTrigger }
+
